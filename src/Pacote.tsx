03e1e1891b694ffd337a073dc73c4fc1f757190a
ian felix <felix.ian@estudante.ufjf.br>
2025-01-27 20:49:36 -0300

    adiciona nova troca de valores

--- src/Pacote.tsx
+++ src/Pacote.tsx
@@ -1,38 +1,55 @@
-import { useState } from "react"
+import { use, useState } from "react"
 import "./Pacote.css"
 
 export default function Pacote() {
-    const [largura, setLargura] = useState(5);
-    const [altura, setAltura] = useState(5);
-    const [profundidade, setProfundidade] = useState(5);
-    const [peso, setPeso] = useState(200);
+
+    const [pacote, setPacote] = useState({
+        altura: 7,
+        largura: 7,
+        profundidade: 7,
+        peso: 7
+    });
+
+    // const [largura, setLargura] = useState(5);
+    // const [altura, setAltura] = useState(5);
+    // const [profundidade, setProfundidade] = useState(5);
+    // const [peso, setPeso] = useState(200);
 
     function onChange(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.name === "largura") {
-            setLargura(e.target.valueAsNumber)
+            setPacote({
+                ...pacote,
+                largura: e.target.valueAsNumber
+            });
         }
         else if (e.target.name === "altura") {
-            setAltura(e.target.valueAsNumber)
+            const novoPacote = structuredClone(pacote);
+            novoPacote.altura = e.target.valueAsNumber;
+            setPacote(novoPacote);
         }
         else if (e.target.name === "profundidade") {
-            setProfundidade(e.target.valueAsNumber)
+            const novoPacote = structuredClone(pacote);
+            novoPacote.profundidade = e.target.valueAsNumber;
+            setPacote(novoPacote);
         }
         else if (e.target.name === "peso") {
-            setPeso(e.target.valueAsNumber)
+            const novoPacote = structuredClone(pacote);
+            novoPacote.peso = e.target.valueAsNumber;
+            setPacote(novoPacote);
         }
     }
 
 
     return <dl className="pacote">
         <dt>Largura (cm)</dt>
-        <dd><input type="number" value={largura} min="5" step="1" onChange={onChange} name="largura" /></dd>
+        <dd><input type="number" value={pacote.largura} min="5" step="1" onChange={onChange} name="largura" /></dd>
         <dt>Altura (cm)</dt>
-        <dd><input type="number" value={altura} min="5" step="1" onChange={onChange} name="altura" /></dd>
+        <dd><input type="number" value={pacote.altura} min="5" step="1" onChange={onChange} name="altura" /></dd>
         <dt>Profundidade (cm)</dt>
-        <dd><input type="number" value={profundidade} min="5" step="1" onChange={onChange} name="profundidade" /></dd>
+        <dd><input type="number" value={pacote.profundidade} min="5" step="1" onChange={onChange} name="profundidade" /></dd>
         <dt>Peso (g)</dt>
-        <dd><input type="number" value={peso} min="200" step="1" onChange={onChange} name="peso" /></dd>
+        <dd><input type="number" value={pacote.peso} min="200" step="1" onChange={onChange} name="peso" /></dd>
         <dt>Custo</dt>
-        <dd>{(largura * peso * altura * profundidade * 0.0005).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</dd>
+        <dd>{(pacote.largura * pacote.peso * pacote.altura * pacote.profundidade * 0.0005).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</dd>
     </dl>
 }
